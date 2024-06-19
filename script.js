7400050131321ea0118619c88b07e07036b787ff
kato0914 <88483254+kato0914@users.noreply.github.com>
2024-06-19 14:07:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-HTML CSS JSResult Skip Results Iframe
-EDIT ON
+window.addEventListener("load", function() {
+    // プラグインを定義
+    gsap.registerPlugin(ScrollTrigger);
 
-   window.addEventListener("load", function(){
+    const area  = document.querySelector(".js-area");
+    const wrap  = document.querySelector(".js-wrap");
+    const items = document.querySelectorAll(".js-item");
+    const num   = items.length;
 
-//プラグインを定義
-gsap.registerPlugin(ScrollTrigger);
+    // 横幅を指定
+    gsap.set(wrap,  { width: num * 100 + "%" });
+    gsap.set(items, { width: 100 / num + "%" });
 
-const area  = document.querySelector(".js-area");
-const wrap  = document.querySelector(".js-wrap");
-const items = document.querySelectorAll(".js-item");
-const num   = items.length;
-
-gsap.set(wrap,  { width: num * 100 + "%" });//横幅を指定
-gsap.set(items, { width: 100 / num + "%" });//横幅を指定
-
-gsap.to(items, {
-  xPercent: -100 * ( num - 1 ), //x方向に移動させる
-  ease: "none",
-  scrollTrigger: {
-    trigger: area, //トリガー
-    start: "top top", //開始位置
-    end: "+=1000", //終了位置 スクロール量の幅調整
-    pin: true, //ピン留め
-    scrub: true, //スクロール量に応じて動かす
-  }
-});
+    gsap.to(wrap, {
+        xPercent: -100 * (num - 1), // x方向に移動させる
+        ease: "none",
+        scrollTrigger: {
+            trigger: area, // トリガー
+            start: "top top", // 開始位置
+            end: "+=1000", // 終了位置 スクロール量の幅調整
+            pin: true, // ピン留め
+            scrub: true, // スクロール量に応じて動かす
+        }
+    });
 });

4b1e5278828578ced7ebd81f347180af3a778055
kato0914 <88483254+kato0914@users.noreply.github.com>
2024-06-19 14:35:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-window.addEventListener("load", function() {
-    // プラグインを定義
-    gsap.registerPlugin(ScrollTrigger);
-HTML CSS JSResult Skip Results Iframe
-EDIT ON
+const stickySections = [...document.querySelectorAll('.sticky_wrap')]
 
-    const area  = document.querySelector(".js-area");
-    const wrap  = document.querySelector(".js-wrap");
-    const items = document.querySelectorAll(".js-item");
-    const num   = items.length;
-   window.addEventListener("load", function(){
+window.addEventListener('scroll', (e) => {
+  for(let i = 0; i < stickySections.length; i++){
+    transform(stickySections[i])
+  }
+})
 
-    // 横幅を指定
-    gsap.set(wrap,  { width: num * 100 + "%" });
-    gsap.set(items, { width: 100 / num + "%" });
-//プラグインを定義
-gsap.registerPlugin(ScrollTrigger);
+function transform(section) {
 
-    gsap.to(wrap, {
-        xPercent: -100 * (num - 1), // x方向に移動させる
-        ease: "none",
-        scrollTrigger: {
-            trigger: area, // トリガー
-            start: "top top", // 開始位置
-            end: "+=1000", // 終了位置 スクロール量の幅調整
-            pin: true, // ピン留め
-            scrub: true, // スクロール量に応じて動かす
-        }
-    });
-});
+  const offsetTop = section.parentElement.offsetTop;
+
+  const scrollSection = section.querySelector('.horizontal_scroll')
+
+  let percentage = ((window.scrollY - offsetTop) / window.innerHeight) * 100;
+
+  percentage = percentage < 0 ? 0 : percentage > 300 ? 300 : percentage;
+
+  scrollSection.style.transform = `translate3d(${-(percentage)}vw, 0, 0)`
+}
